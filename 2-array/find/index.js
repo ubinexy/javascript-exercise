3a008fe366817c9b61ccdea4ad7d8c6dde219d90
ubinexy <ubinexy@gmail.com>
2020-08-10 16:22:34 +0800

    [shiqi]: array exercise

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,4 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
+  return collection.find(x => x.age < 21).name;
 }
--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,4 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  return collection.map(x => String.fromCharCode(x + 96));
 }
