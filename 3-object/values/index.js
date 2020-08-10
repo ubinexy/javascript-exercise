c6f280ef0dcdee8aa66bfe3a1a6a33b0d4275eef
ubinexy <ubinexy@gmail.com>
2020-08-10 20:52:06 +0800

    [shiqi] lint: add missing radix prameter

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,4 +1,4 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
-  return Object.values(source).reduce((a, b) => parseInt(a) + parseInt(b));
+  return Object.values(source).reduce((a, b) => parseInt(a, 0) + parseInt(b, 0));
 }
