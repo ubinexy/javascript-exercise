3ae4e04b69c0c462439caf299dece9743e213506
ubinexy <ubinexy@gmail.com>
2020-08-10 20:48:51 +0800

    [shiqi] class practice 4

--- 4-class/practice_4/worker.js
+++ 4-class/practice_4/worker.js
@@ -1,1 +1,8 @@
 // TODO 17: 在这里写实现代码
+import Person from './person';
+
+export default class Worker extends Person {
+  introduce() {
+    return `${this.basicIntroduce()} I am a Worker. I have a job.`;
+  }
+}
